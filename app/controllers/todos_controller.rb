1adfcb4006e37ed9163be2d8b5ae21ed6bb6f4eb
Yujin Won <iamyujinwon@gmail.com>
2022-11-01 17:48:31 -0400

    complete todo list

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,7 +3,7 @@ class TodosController < ApplicationController
 
   # GET /todos or /todos.json
   def index
-    @todos = Todo.all
+    @todos = Todo.in_order_of(:status, %w[incomplete complete])
   end
 
   # GET /todos/1 or /todos/1.json
@@ -25,11 +25,10 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
+        format.turbo_stream 
         format.html { redirect_to todo_url(@todo), notice: "Todo was successfully created." }
-        format.json { render :show, status: :created, location: @todo }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@todo)}_form", partial: "form", locals: { todo: @todo})}
       end
     end
   end
@@ -52,6 +51,7 @@ class TodosController < ApplicationController
     @todo.destroy
 
     respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove("#{helpers.dom_id(@todo)}_item") }
       format.html { redirect_to todos_url, notice: "Todo was successfully destroyed." }
       format.json { head :no_content }
     end
